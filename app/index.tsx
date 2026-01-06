0c1d0c2850c7106b9bc3174eb015adc6675c493a
wktlaghk-boop <wktlaghk@gmail.com>
2026-01-06 17:16:33 +0900

    기본 컴포넌트, 스타일 컴포넌트 사용방법

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,33 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World</Text>
+      <Text style={styles.mainText}>Hello World!</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          borderWidth: 1,
+          borderColor: "blue",
+          color: "blue",
+          padding: 10,
+          margin: 10,
+          borderRadius: 10,
+        }}
+      >
+        Hello World!
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+});
