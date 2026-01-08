7b919020d7c8851919777f60c39c2cf8b341fccb
wktlaghk-boop <wktlaghk@gmail.com>
2026-01-08 15:11:14 +0900

    flex column

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,9 @@ export default function Index() {
   return (
     <>
       <View style={[styles.container]}>
-        <View style={{ flex: 1, backgroundColor: "red" }} />
-        <View style={{ flex: 2, backgroundColor: "darkorange" }} />
-        <View style={{ flex: 3, backgroundColor: "green" }} />
+        <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
+        <View style={{ width: 100, height: 100, backgroundColor: "blue" }} />
+        <View style={{ width: 100, height: 100, backgroundColor: "green" }} />
       </View>
     </>
   );
@@ -15,6 +15,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //flexDirection: "row", 가로배열(column은 세로배열 이지만 기본값이므로 생략가능)
+    //alignItems: "center",
+    justifyContent: "center",
   },
 });
