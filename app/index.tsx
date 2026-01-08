86e3df245aefc0bebfffc82ce3da05eac8d078bc
wktlaghk-boop <wktlaghk@gmail.com>
2026-01-08 14:40:45 +0900

    image, textInput 사용

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
     <>
       <View style={styles.container}>
         <Text style={styles.mainText}>Hello World!</Text>
+        {/* <Image source={require("../assets/images/logo.png")} /> 경로이미지 */}
+        <Image
+          source={{ uri: "https://picsum.photos/id/237/200/300" }}
+          style={{ width: 200, height: 200 }}
+          resizeMode="cover"
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="이름을 입력해주세요."
+          placeholderTextColor="#000"
+        />
       </View>
-      <View style={styles.container2}></View>
     </>
   );
 }
@@ -16,19 +26,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
-  },
-  container2: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "blue",
   },
   mainText: {
     fontSize: 20,
     fontWeight: "bold",
     borderWidth: 1,
     borderColor: "red",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "#000",
+    backgroundColor: "#fff",
+    width: 200,
     padding: 10,
     margin: 10,
     borderRadius: 10,
